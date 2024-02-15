6c052396fe4069ea1f991c06084699709bde7fad
chiarajugno <jugnochiara@gmail.com>
2024-02-16 04:08:35 +0800

    Create profile.js

---
+++ js/profile.js
@@ -0,0 +1,10 @@
+
+function openPopUp(){
+  let popUp = document.getElementById("edit-popup");
+  popUp.style.visibility = 'visible';
+}
+
+function closePopUp(){
+  let popUp = document.getElementById("edit-popup");
+  popUp.style.visibility = 'hidden';
+}
