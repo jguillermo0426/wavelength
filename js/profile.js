932c9f5369f099a5ad905f48a14fc86ef8772b55
chiarajugno <jugnochiara@gmail.com>
2024-02-16 12:10:01 +0800

    added edit/delete profile post on click

--- js/profile.js
+++ js/profile.js
@@ -8,3 +8,15 @@ function closePopUp(){
   let popUp = document.getElementById("edit-popup");
   popUp.style.visibility = 'hidden';
 }
+
+function openEditDelete(postID) {
+  let editDelete = document.getElementById(postID);
+  editDelete.style.visibility = 'visible';
+  editDelete.style.left = pageX + 'px';
+  editDelete.style.top = pageY + 'px';
+}
+
+function closeEditDelete(postID) {
+  let editDelete = document.getElementById(postID);
+  editDelete.style.visibility = 'hidden';
+}
